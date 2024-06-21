7dc8e11d719fda5156e88951355e1684bc4eeea3
Motalib01 <abdelmotalib.chemouri@gmail.com>
2024-06-21 20:33:42 +0100

    add model and dto and mappin them using automapper

---
+++ Test/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.Dtos
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
+
---
+++ Test/Helper/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Test.Dtos;
+using Test.Models;
+
+namespace Test.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
+
